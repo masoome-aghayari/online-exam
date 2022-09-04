be84af2ae30e4d439aac81b753edc7a801bf60b6
masoome.aghayari <masoome.aghayari@gmail.com>
2022-09-04 11:55:14 +0430

    add @NoArgsConstructor and @AllArgsConstructor to ResponseModel.java

--- src/main/java/ir/maktab/onlineexam/model/dto/ResponseModel.java
+++ src/main/java/ir/maktab/onlineexam/model/dto/ResponseModel.java
@@ -1,12 +1,12 @@
 package ir.maktab.onlineexam.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseModel<T> {
     private T data;
-
-    public ResponseModel(T data) {
-        this.data = data;
-    }
 }
